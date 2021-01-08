e4ac0a8893e62d3d0fc2355edf8f3a8474eef5c0
Beidan Huang <huangbeidan@gmail.com>
2021-01-07 16:16:50 -0800

    create and configure first example

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from active_learner.lsh_analyzer import LSHAnalyzer
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    analyzer = LSHAnalyzer()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
